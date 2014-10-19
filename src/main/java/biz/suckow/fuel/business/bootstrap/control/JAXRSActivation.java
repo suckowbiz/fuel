8971e84e1b09142ae1a3240cded5b8150458723c
Tobias <tobias@suckow.biz>
2014-10-19 18:40:46 +0200

    Refactoring.

---
+++ src/main/java/biz/suckow/fuel/business/bootstrap/control/JAXRSActivation.java
@@ -0,0 +1,10 @@
+package biz.suckow.fuel.business.bootstrap.control;
+
+import javax.persistence.Access;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSActivation extends Application{
+
+}
