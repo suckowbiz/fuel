014af3ccd3f825a992a4e9577d2c55cd7b5b840f
Tobias <tobias@suckow.biz>
2014-11-16 17:04:11 +0100

    Introduction integration test for refueling locator. Fixed query.

---
+++ src/test/java/biz/suckow/fuel/business/consumption/control/RefuelingLocatorIT.java
@@ -0,0 +1,89 @@
+package biz.suckow.fuel.business.consumption.control;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.testng.annotations.Test;
+
+import biz.suckow.fuel.business.ArquillianBase;
+import biz.suckow.fuel.business.owner.entity.Owner;
+import biz.suckow.fuel.business.refueling.entity.Refueling;
+import biz.suckow.fuel.business.vehicle.entity.Vehicle;
+
+public class RefuelingLocatorIT extends ArquillianBase {
+    @Inject
+    private RefuelingLocator cut;
+
+    @Inject
+    private EntityManager em;
+
+    @Test
+    @Transactional(value = TransactionMode.ROLLBACK)
+    public void testLocateRefuelingsByDateAndStatus() {
+        final Owner duke = new Owner().setOwnername("duke");
+        this.em.persist(duke);
+
+        final Vehicle dukeCar = new Vehicle().setOwner(duke).setVehiclename("duke-car");
+        this.em.persist(dukeCar);
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 0);
+        final Date january = calendar.getTime();
+
+        calendar.set(Calendar.MONTH, 1);
+        final Date february = calendar.getTime();
+
+        calendar.set(Calendar.MONTH, 2);
+        final Date march = calendar.getTime();
+
+        Refueling refueling = new Refueling.Builder()
+                .dateRefueled(january)
+                .eurosPerLitre(1D)
+                .fillUp(true)
+                .litres(1D)
+                .vehicle(dukeCar)
+                .build();
+        this.em.persist(refueling);
+
+        refueling = new Refueling.Builder()
+                .dateRefueled(february)
+                .eurosPerLitre(1D)
+                .fillUp(true)
+                .litres(1D)
+                .vehicle(dukeCar)
+                .build();
+        this.em.persist(refueling);
+
+        refueling = new Refueling.Builder()
+                .dateRefueled(march)
+                .eurosPerLitre(1D)
+                .fillUp(true)
+                .litres(1D)
+                .vehicle(dukeCar)
+                .build();
+        this.em.persist(refueling);
+
+        final Refueling refuelingMarchPartial = new Refueling.Builder()
+                .dateRefueled(march)
+                .eurosPerLitre(1D)
+                .fillUp(false)
+                .litres(1D)
+                .vehicle(dukeCar)
+                .build();
+        this.em.persist(refuelingMarchPartial);
+
+        final List<Refueling> actualResult = this.cut.getFilledUpRefuelingsMissingConsumptionOldestFirst();
+        assertThat(actualResult.size(), is(3));
+        assertThat(actualResult.get(0).getDateRefueled(), is(march));
+        assertThat(actualResult.get(1).getDateRefueled(), is(february));
+        assertThat(actualResult.get(2).getDateRefueled(), is(january));
+    }
+}
