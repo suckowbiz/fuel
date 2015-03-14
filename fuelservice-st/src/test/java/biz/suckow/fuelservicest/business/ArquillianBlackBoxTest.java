5df986c10c0afa6aa93f79d6ca32dad1ecd096a3
Tobias <tobias@suckow.biz>
2015-03-14 12:50:13 +0100

    Simplified .war name.

--- fuelservice-st/src/test/java/biz/suckow/fuelservicest/business/ArquillianBlackBoxTest.java
+++ fuelservice-st/src/test/java/biz/suckow/fuelservicest/business/ArquillianBlackBoxTest.java
@@ -33,14 +33,14 @@ import javax.ws.rs.client.WebTarget;
 
 @ArquillianSuiteDeployment
 public abstract class ArquillianBlackBoxTest extends Arquillian {
-    private final String BASE = "http://localhost:8080/fuelservice-0.0.1-SNAPSHOT/resources/";
+    private final String BASE = "http://localhost:8080/fuelservice/resources/";
     protected final WebTarget target = ClientBuilder.newClient().target(BASE);
 
     @Deployment(testable = false)
     @OverProtocol("Servlet 3.0")
     public static WebArchive deploy() {
         final WebArchive war = ShrinkWrap.create(MavenImporter.class)
-                .loadPomFromFile("../fuelservice/pom.xml").importBuildOutput().as(WebArchive.class);
+                .loadPomFromFile("../fuelservice/pom.xml").importBit uildOutput().as(WebArchive.class);
         war.delete("META-INF/persistence.xml");
         war.addAsResource("arquillian/persistence.xml", "META-INF/persistence.xml");
         return war;
